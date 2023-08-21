04d4ff37140f5c1d137b232365ef3f861dabcc6b
Mete Sayan <metesayan99@gmail.com>
2023-08-21 22:28:12 +0300

    String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-exercises';
+  title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  togglePokemon: boolean = false;
+
+  constructor() {
+    this.title = 'Mete';
+  }
 }
