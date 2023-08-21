7fc99f677ede304cf96ae94f7a547896c5ce5bd5
Mete Sayan <metesayan99@gmail.com>
2023-08-22 01:01:55 +0300

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,39 @@ import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string;
+  isCool: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  pokemonName: string;
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'pikachu',
+      type: 'electric',
+      isCool: true,
+    },
+    {
+      id: 2,
+      name: 'squirtle',
+      type: 'water',
+      isCool: false,
+    },
+    {
+      id: 3,
+      name: 'charmander',
+      type: 'fire',
+      isCool: true,
+    },
+  ];
 
-  constructor() {
-    this.pokemonName = '';
-  }
-
-  handleClick(value: any) {
-    console.log(value);
-  }
+  constructor() {}
 }
