943679b09df349514ba2ff4deecd32cf66e7240e
Mete Sayan <metesayan99@gmail.com>
2023-08-22 17:11:46 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,29 @@ interface Pokemon {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  pokemonName: string = '';
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'pikachu',
+      type: 'electric',
+      isCool: true,
+      isStylish: true,
+    },
+    {
+      id: 2,
+      name: 'squirtle',
+      type: 'water',
+      isCool: false,
+      isStylish: true,
+    },
+    {
+      id: 3,
+      name: 'charmander',
+      type: 'fire',
+      isCool: true,
+      isStylish: false,
+    },
+  ];
 
   constructor() {}
-
-  handleChange(event: any) {
-    this.pokemonName = event.target.value;
-  }
 }
