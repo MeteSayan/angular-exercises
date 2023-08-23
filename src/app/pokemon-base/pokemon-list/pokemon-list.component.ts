ca4dc0179ea42e285d867483956309bd27edc296
Mete Sayan <metesayan99@gmail.com>
2023-08-23 17:53:56 +0300

    OnInit with pokemon-list component

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit {
+  pokemon: string;
+  constructor() {
+    this.pokemon = 'pikachu';
+  }
 
+  ngOnInit(): void {
+    console.log('ngOnInit fired -', this.pokemon);
+  }
 }
