4333b536beeb003e1a7bd638acd157f0ee2029af
Mete Sayan <metesayan99@gmail.com>
2023-08-23 15:13:52 +0300

    feat: pokemon-base module created with pokemon-list component

--- README.md
+++ README.md
@@ -25,3 +25,15 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Hints
+
+```bash
+# Generate Module
+$ ng generate module pokemon-base
+```
+
+```bash
+# Generate Component
+$ ng generate component pokemon-base/pokemon-list --module=pokemon-base/pokemon-base.module.ts
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, PokemonBaseModule],
   providers: [],
   bootstrap: [AppComponent],
 })
